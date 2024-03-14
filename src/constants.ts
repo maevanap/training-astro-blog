44fe0ea23690ad5218c07d25bfe4430d004aa983
maevanap <maevanap.contact@gmail.com>
2024-03-14 22:45:53 +0100

    Collections

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "TechPeople Blog";
 export const SITE_DESCRIPTION = "Articles, Stories & Tutorials for Tech People";
-export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const HOMEPAGE_ARTICLE_LIMIT = 3;
 export const ARTICLES_PER_PAGE = 6;
