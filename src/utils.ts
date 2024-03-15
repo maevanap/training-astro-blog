ec8fb6c3b442af96198c01e2956550bebcef2c98
maevanap <maevanap.contact@gmail.com>
2024-03-15 10:28:18 +0100

    routes, slugs and tags

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import type { CollectionEntry } from "astro:content";
+
 function formatDate(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
@@ -8,4 +10,15 @@ function formatDate(date: Date): string {
   return new Date(date).toLocaleDateString(undefined, options);
 }
 
-export { formatDate };
+function capitalize(str: string): string {
+  return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+}
+
+function sortBlogArticlesByDate(articles: CollectionEntry<"blog">[]): CollectionEntry<"blog">[] {
+  return articles.sort(
+    (a: CollectionEntry<"blog">, b: CollectionEntry<"blog">) =>
+      b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+  );
+}
+
+export { formatDate, capitalize, sortBlogArticlesByDate };
